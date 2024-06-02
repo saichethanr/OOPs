2ec0a5ced47526602aa7389a41bd55d7f1b508f5
saichethanr <saichethan9019@gmail.com>
2024-06-02 13:23:29 +0530

    multi level inheritance

---
+++ inheritance/multilevelinheritance.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+//A-->B-->C
+class Animal{
+  public:
+  int age;
+  int height;
+  int weight;
+  private:
+  int calm;
+  
+  public:
+  void speak(){
+    cout<<"speaking"<<endl;
+  }
+  int getAge(){
+    return this->age;
+  }
+
+  void setWeight(int w){
+    this->weight = w;
+  }
+
+};
+
+//all attributes of the humanclass is inerited nside the male cllass
+class male : public Animal{
+    public:
+    string color;
+
+    void sleep(){
+        cout<<"male sleeping"<<endl;
+    }
+};
+
+class female : public male{
+    public:
+    string color;
+
+    void sleep(){
+        cout<<"male sleeping"<<endl;
+    }
+};
+
+
+
+int main(){
+
+  //public --> public  = public
+    female f;
+  f.speak();
+
+
+}
+
